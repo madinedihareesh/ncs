e04251d932413f422388461bff9fe3237b2ea87f
Hareesh <hareesh.madinedi@gmail.com>
2025-04-03 13:48:04 +0530

    Form validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 // import { useState } from 'react'
 import './App.css'
-import Crender from './Crender'
+// import Crender from './Crender'
 // import With from './With'
 // import Without from './Without'
 // import Make from './styles.module.css'
@@ -12,10 +12,45 @@ import Crender from './Crender'
 
 
 function App() {
-  
+  let email="";
+  let password="";
+
+  function handleEmailChange(event){
+     email=event.target.value;
+    }
+
+  function handlePasswordChange(event){
+     password=event.target.value;
+
+  } 
+  function handleSubmit(event){
+    event.preventDefault();
+    const emailPatern=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordPatern=/^[A-Za-z0-9]{6,}$/;
+
+    if(!emailPatern.test(email)){
+      alert("Incorrect Email");
+      return;
+    }
+    if(!passwordPatern.test(password)){
+      alert("Incorrect password");
+      return;
+    }
+    alert("Login secuss")
+
+  } 
   return (
     <>
-    <Crender/>
+     <h1>Log-in Form</h1>
+     <form action="#" onSubmit={handleSubmit}>
+      <input type="email" 
+      placeholder='Enter the eamil'
+      onChange={handleEmailChange}/>
+      <input type="password" 
+      placeholder='Enter the password'
+      onChange={handlePasswordChange}/>
+      <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
